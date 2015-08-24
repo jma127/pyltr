96e2934e2ff41e667db9359ba5f30f5ba8321adc
Jerry Ma <jerryma@nospam>
2015-08-23 19:11:00 -0700

    Yield style changes

--- pyltr/metrics/tests/test_ap.py
+++ pyltr/metrics/tests/test_ap.py
@@ -15,15 +15,15 @@ class TestAP(helpers.TestMetric):
         return pyltr.metrics.AP(k=4)
 
     def get_queries_with_values(self):
-        yield ([], 0.0)
-        yield ([0], 0.0)
-        yield ([1], 1.0)
-        yield ([1, 0], 1.0)
-        yield ([0, 1], 0.5)
-        yield ([1, 0, 1, 0], 5.0 / 6)
-        yield ([0, 1, 1, 1], 23.0 / 36)
-        yield ([1, 0, 1, 0, 1], 5.0 / 6)
-        yield ([1, 0, 1, 0, 0], 5.0 / 6)
+        yield [], 0.0
+        yield [0], 0.0
+        yield [1], 1.0
+        yield [1, 0], 1.0
+        yield [0, 1], 0.5
+        yield [1, 0, 1, 0], 5.0 / 6
+        yield [0, 1, 1, 1], 23.0 / 36
+        yield [1, 0, 1, 0, 1], 5.0 / 6
+        yield [1, 0, 1, 0, 0], 5.0 / 6
 
     def get_queries(self):
         for i in range(0, 7):
--- pyltr/metrics/tests/test_dcg.py
+++ pyltr/metrics/tests/test_dcg.py
@@ -15,14 +15,14 @@ class TestDCG(helpers.TestMetric):
         return pyltr.metrics.DCG(k=3)
 
     def get_queries_with_values(self):
-        yield ([], 0.0)
-        yield ([0], 0.0)
-        yield ([1], 1.0)
-        yield ([2], 3.0)
-        yield ([2, 1, 0], 3.6309297535714578)
-        yield ([0, 0, 0], 0.0)
-        yield ([2, 5, 1], 23.058822360715183)
-        yield ([2, 5, 1, 9], 23.058822360715183)
+        yield [], 0.0
+        yield [0], 0.0
+        yield [1], 1.0
+        yield [2], 3.0
+        yield [2, 1, 0], 3.6309297535714578
+        yield [0, 0, 0], 0.0
+        yield [2, 5, 1], 23.058822360715183
+        yield [2, 5, 1, 9], 23.058822360715183
 
     def get_queries(self):
         for i in range(0, 5):
@@ -35,16 +35,16 @@ class TestNDCG(helpers.TestMetric):
         return pyltr.metrics.NDCG(k=3)
 
     def get_queries_with_values(self):
-        yield ([], 0.0)
-        yield ([0], 0.0)
-        yield ([1], 1.0)
-        yield ([2], 1.0)
-        yield ([2, 1, 0], 1.0)
-        yield ([1, 2, 0], 0.7967075809905066)
-        yield ([0, 0, 0], 0.0)
-        yield ([2, 5, 1], 0.6905329824556825)
-        yield ([2, 5, 1, 9], 0.04333885914794999)
-        yield ([3, 2, 1, 1], 1.0)
+        yield [], 0.0
+        yield [0], 0.0
+        yield [1], 1.0
+        yield [2], 1.0
+        yield [2, 1, 0], 1.0
+        yield [1, 2, 0], 0.7967075809905066
+        yield [0, 0, 0], 0.0
+        yield [2, 5, 1], 0.6905329824556825
+        yield [2, 5, 1, 9], 0.04333885914794999
+        yield [3, 2, 1, 1], 1.0
 
     def get_queries(self):
         for i in range(0, 5):
