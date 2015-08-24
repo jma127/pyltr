07b587849d2cdc2e0c385bfaa937164865a68cfe
Jerry Ma <jerryma@nospam>
2015-08-23 18:20:55 -0700

    Tests for average precision

---
+++ pyltr/metrics/tests/helpers.py
@@ -0,0 +1,69 @@
+"""
+
+Test helpers for metric classes.
+
+Essentially, all you need to do is:
+
+- Define a small set of queries along with expected metric values (for testing
+  evaluate())
+- Define a larger set of queries for testing other methods, using evaluate() as
+  ground truth.
+
+"""
+
+import numpy as np
+
+from numpy.testing import assert_approx_equal, assert_array_almost_equal_nulp
+
+
+class TestMetric(object):
+    def get_metric(self):
+        """Metric class instance to test."""
+        raise NotImplementedError()
+
+    def get_queries_with_values(self):
+        """Queries to test for evaluate()."""
+        raise NotImplementedError()
+
+    def get_queries(self):
+        """Queries to test for methods except evaluate()."""
+        raise NotImplementedError()
+
+    def test_evaluate(self):
+        m = self.get_metric()
+        for id, (q, v) in enumerate(self.get_queries_with_values()):
+            # Test twice to ensure caching doesn't break things.
+            assert_approx_equal(v, m.evaluate(id, q))
+            assert_approx_equal(v, m.evaluate(id, q))
+
+    def test_calc_swap_deltas(self):
+        m = self.get_metric()
+        for id, q in enumerate(self.get_queries()):
+            n = len(q)
+            q_cpy = np.copy(q)
+            expected_deltas = np.zeros((n, n))
+            deltas = m.calc_swap_deltas(id, q)
+            orig = m.evaluate(id, q)
+
+            for i in range(n):
+                for j in range(n):
+                    q_cpy[[i, j]] = q[[j, i]]
+                    expected_deltas[i, j] = m.evaluate(id, q_cpy) - orig
+                    q_cpy[[i, j]] = q[[i, j]]
+
+            # Test that diagonal is 0.
+            assert_array_almost_equal_nulp(
+                0.0, np.diagonal(expected_deltas), nulp=99)
+
+            # Test that the expected deltas are symmetric.
+            assert_array_almost_equal_nulp(
+                expected_deltas.transpose(), expected_deltas, nulp=99)
+
+            # Test that deltas computed by the metric match the deltas from
+            # evaluate().
+            assert_array_almost_equal_nulp(
+                np.triu(expected_deltas), deltas, nulp=99)
+
+            # Test that max_k() works.
+            assert_array_almost_equal_nulp(
+                0.0, expected_deltas[m.max_k():, m.max_k():], nulp=99)
---
+++ pyltr/metrics/tests/test_ap.py
@@ -0,0 +1,31 @@
+"""
+
+Testing for (Mean) Average Precision metric.
+
+"""
+
+import helpers
+import itertools
+import numpy as np
+import pyltr
+
+
+class TestAP(helpers.TestMetric):
+    def get_metric(self):
+        return pyltr.metrics.AP(k=4)
+
+    def get_queries_with_values(self):
+        yield ([], 0.0)
+        yield ([0], 0.0)
+        yield ([1], 1.0)
+        yield ([1, 0], 1.0)
+        yield ([0, 1], 0.5)
+        yield ([1, 0, 1, 0], 5.0 / 6)
+        yield ([0, 1, 1, 1], 23.0 / 36)
+        yield ([1, 0, 1, 0, 1], 5.0 / 6)
+        yield ([1, 0, 1, 0, 0], 5.0 / 6)
+
+    def get_queries(self):
+        for i in range(0, 7):
+            for tup in itertools.product(*([(0, 1)] * i)):
+                yield np.array(tup)
