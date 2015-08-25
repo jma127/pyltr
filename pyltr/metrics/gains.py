3017e6499d2b4cffe399173dc1717959ccc36c56
Jerry Ma <jerryma@nospam>
2015-08-24 17:31:58 -0700

    Gains docstrings

--- pyltr/metrics/gains.py
+++ pyltr/metrics/gains.py
@@ -10,17 +10,34 @@ import math
 _LOG2 = math.log(2.0)
 
 
-def identity_gain(x):
+def _identity_gain(x):
     return x
 
 
-def exp2_gain(x):
+def _exp2_gain(x):
     return math.exp(x * _LOG2) - 1.0
 
 
 def get_gain_fn(name, **args):
+    """Returns a gain callable corresponding to the provided gain name.
+
+    Parameters
+    ----------
+    name : {'identity', 'exp2'}
+        Name of the gain to return.
+
+        - identity: ``lambda x : x``
+
+        - exp2: ``lambda x : (2.0 ** x) - 1.0``
+
+    Returns
+    -------
+    gain_fn : callable
+        Callable that returns the gain of target values.
+
+    """
     if name == 'identity':
-        return identity_gain
+        return _identity_gain
     elif name == 'exp2':
-        return exp2_gain
+        return _exp2_gain
     raise ValueError(name + ' is not a valid gain type')
