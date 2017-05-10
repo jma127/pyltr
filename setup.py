07f4458990962f66d68c7c122a27b10ce66f84f6
Jerry Ma <jma127@users.noreply.github.com>
2017-05-09 21:07:15 -0400

    Change author email

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.2.0',
     description='Python learning to rank (LTR) toolkit.',
     author='Jerry Ma',
-    author_email='jerryma_nospam@example.org',
+    author_email='jma127@users.noreply.github.com',
     url='https://github.com/jma127/pyltr',
     license='BSD-new',
 
