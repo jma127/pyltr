159c2d9e88cee93ca02bb35ad1e622683bc1a27e
Jerry Ma <jmnospam@mail.com>
2017-06-29 09:16:01 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyltr',
-    version='0.2.1',
+    version='0.2.2',
     description='Python learning to rank (LTR) toolkit.',
     author='Jerry Ma',
     author_email='jmnospam@mail.com',
