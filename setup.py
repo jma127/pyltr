c419840432a3c33bc29eece06a679888f1624f46
Jerry Ma <noreplyspamblackhole@gmail.com>
2020-09-22 23:52:26 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyltr',
-    version='0.2.5',
+    version='0.2.6',
     description='Python learning to rank (LTR) toolkit.',
     author='Jerry Ma',
     author_email='jmnospam@mail.com',
